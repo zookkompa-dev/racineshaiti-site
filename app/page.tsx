4330d3f8942ca519730af4cb957cb3cac4429aa3
zookkompa-dev <zookkompa@gmail.com>
2026-08-06 22:52:39 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/components/Hero";
+import Heroes from "@/components/Heroes";
+import Timeline from "@/components/Timeline";
+import Culture from "@/components/Culture";
+import VideoSection from "@/components/VideoSection";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Heroes />
+      <Timeline />
+      <Culture />
+      <VideoSection />
+    </>
+  );
+}
